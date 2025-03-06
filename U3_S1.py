9430bd3a194836bab1d984aa15e2cc52fa78d157
Phyo Thant <pthant@go.pasadena.edu>
2025-03-05 20:33:40 -0800

    u3

---
+++ U3_S1.py
@@ -0,0 +1,19 @@
+# testing123 with stack
+def checkParenBalance(s):
+    if len(s) == 0:
+        return False
+
+    stack = []
+    
+    for char in s:
+        if char == "(":
+            stack.append("(")
+        elif char == ")":
+            if len(stack) > 0:
+                stack.pop()
+            else:
+                return False
+    return len(stack) == 0
+
+string1 = "((()))"
+print (f"{string1} -> {checkParenBalance(string1)}")
